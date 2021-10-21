e352af5d222657e4f8ecdebb376a8c7694e355fe
libaohui <libaohui@100tal.com>
2021-10-21 20:44:43 +0800

    leetcode练习

--- src/main/java/com/example/JUC/U8/Test_ReentrantReadWriteLock.java
+++ src/main/java/com/example/JUC/U8/Test_ReentrantReadWriteLock.java
@@ -59,3 +59,38 @@ class DataContainer{
         }
     }
 }
+
+@Slf4j(topic = "c.CacheData")
+class CacheData{
+    Object data;
+    //是否有效，如果失败，需要重新计算data
+    volatile boolean cacheValid;
+    final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+
+    void processCachedData(){
+        rwl.readLock().lock();
+        if(!cacheValid){
+            //获取写锁前，必须释放读锁
+            rwl.readLock().unlock();
+            rwl.writeLock().lock();
+            try{
+                //判断是否有其他线程已经获取了写锁，更新了缓存，避免重复更新
+                if(!cacheValid){
+                    data = new Object();
+                    cacheValid = true;
+                }
+                //降级为读锁，释放写锁，这样能够让其他线程读取缓存
+                rwl.readLock().lock();
+            }finally {
+                rwl.writeLock().unlock();
+            }
+        }
+        //自己用完数据，释放读锁
+        try{
+            //use(data);
+        }finally {
+            rwl.readLock().unlock();
+        }
+    }
+}
+
